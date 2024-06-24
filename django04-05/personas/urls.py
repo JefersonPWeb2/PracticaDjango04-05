4c8c24298130b574ebf8a9b4732803a8042332bf
Jeferson <jbasurco@unsa.edu.pe>
2024-06-23 23:12:43 -0500

    Agregando ruta para usar ListView

--- django04-05/personas/urls.py
+++ django04-05/personas/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
-from personas.views import (
-    personasTestView,
+from .views import (
     personaCreateView,
     personasShowObject,
     personasDeleteView,
     personasListView,
+    PersonaListView,
 )
 app_name = 'personas'
 urlpatterns = [
     path('add/', personaCreateView, name='adding'),
     path('<int:myID>/', personasShowObject, name = 'browsing'),
     path('<int:myID>/delete/', personasDeleteView, name = 'deleting'),
-    path('', personasListView, name = 'listing')
+    path('', personasListView, name = 'listing'),
+    path('', PersonaListView.as_view(), name = 'persona-list'),
 ]
