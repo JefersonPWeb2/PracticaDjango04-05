469ed11a62479f57dbc885e4aa19c339ae5c556e
Jeferson <jbasurco@unsa.edu.pe>
2024-06-23 23:25:05 -0500

    Corrigiendo errores

--- django04-05/personas/models.py
+++ django04-05/personas/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.urls import reverse
-from django.views.generic.list import ListView
 # Create your models here.
 class Persona(models.Model):
     nombres = models.CharField(max_length=100)
@@ -10,6 +9,3 @@ class Persona(models.Model):
     
     def get_absolute_url(self):
         return reverse('browsing', kwargs={'myID':self.id})
-
-class PersonaListView(ListView):
-    model = Persona
